ad6091b1b285cf5e09740f122a815eedc632ad31
root <anlrey.klochko@mail.ru>
2020-08-11 17:24:04 +0300

    update training page(add training control button)

--- AngularProject/app/components/trainingPage/training.js
+++ AngularProject/app/components/trainingPage/training.js
@@ -1,7 +1,16 @@
 angular.module('myApp.trainings', [])
-    .controller('TrainingsCtrl', function($scope, trainingService, $routeParams,$window) {
+    .controller('TrainingsCtrl', function($scope, trainingService,$window) {
+        $scope.userLogin = localStorage.getItem('UserLogin')
         trainingService.get_trainings().then(function(value){
             $scope.trainings = value
             console.log(value)
         })
+        $scope.check = function(login){
+            let flag = false
+            console.log($scope.userLogin)
+            if($scope.userLogin == login){
+                return true
+            }
+            return flag
+        }
     })
