7c4215546bd83d31d0cbb17509623f15d49f36a4
root <anlrey.klochko@mail.ru>
2020-08-11 15:53:25 +0300

    some fix

--- AngularProject/app/components/trainingPage/addTrainingPage/addTraining.js
+++ AngularProject/app/components/trainingPage/addTrainingPage/addTraining.js
@@ -8,9 +8,16 @@ angular.module('myApp.trainings.add', [])
                 if($scope.dateOfTraining <=  date || $scope.endOfRegistration <=  date){
                     $scope.message = "Incorrect date"
                 }
-                if($scope.dateOfTraining < $scope.endOfRegistration){
+                else if($scope.dateOfTraining < $scope.endOfRegistration){
                     $scope.message = "The training date must be later than the registration date "
-                    console.log("lol")
+                }
+                else {
+                    const training = {
+                        date: $scope.dateOfTraining,
+                        dateOfEnd: $scope.endOfRegistration,
+                        type: $scope.type,
+                        numberOfSeats: $scope.numberOfSeats
+                    }
                 }
             }
         }
